ace0f409fc03f932f5392315c33b8f197c6a4c8c
joachim <ageron.joachim@gmail.com>
2025-04-25 19:28:36 +0200

    ✨ Configure Vite server settings in plugins.ts
    - Set host to "0.0.0.0" for external access
    - Define port as 1337
    - Allow access from specified domains

--- audelweiss-api/config/plugins.ts
+++ audelweiss-api/config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default () => ({
+  vite: {
+    // Ajoutez cette section pour configurer Vite
+    server: {
+      host: "0.0.0.0",
+      port: 1337,
+      // Autoriser l'accès depuis votre domaine
+      allowedHosts: ["audelweiss.ageronjoachim.com", "localhost"],
+    },
+  },
+});

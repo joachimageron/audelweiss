d6182f77ef3594459cc5a4d7cbd5eb957205abdc
Louis-Cauvet <louiscauvet8@gmail.com>
2025-06-20 16:47:47 +0200

    🎨 Adding Tailwind Variants' classes for megamenu

--- audelweiss-front/src/components/baseElements/Navigation.tsx
+++ audelweiss-front/src/components/baseElements/Navigation.tsx
@@ -14,7 +14,6 @@ import CustomInputField from "@/src/components/atoms/CustomInputField";
 import { Burger, Close, Search } from "@/src/components/icons";
 
 import { tv } from "tailwind-variants";
-import clsx from "clsx";
 
 
 const styles = tv({
@@ -23,8 +22,13 @@ const styles = tv({
     navList: "flex flex-col lg:items-center lg:flex-row lg:gap-[2rem] gap-[.5rem] w-full",
     navItem: "flex flex-col group",
     navLink: "nav-link flex items-center lg:gap-[.7rem] gap-[1.2rem] py-[1rem] lg:py-[3.8rem] text-[1.4rem] font-semibold uppercase as--hover-filter-primary transition",
-    subMenu: "lg:absolute lg:left-0 lg:top-[99%] lg:p-[1.5rem] lg:shadow-lg lg:group-hover:opacity-100 lg:group-hover:pointer-events-auto lg:group-focus-within:opacity-100 lg:group-focus-within:pointer-events-auto lg:opacity-0 lg:pointer-events-none min-w-[100%] bg-white flex lg:flex-row flex-col lg:gap-0 gap-[1rem] lg:mb-0 mb-[.6rem] lg:border-t border-t-light-primary transition z-40",
-    subLink: "inline-block lg:px-[1.5rem] lg:mb-[.6rem] lg:px-[2.3rem] px-[1.4rem] py-[1rem] w-full text-[1.5rem] font-semibold uppercase",
+    megaMenu: "lg:absolute lg:left-0 lg:top-[99%] lg:p-[1.5rem] lg:shadow-lg lg:group-hover:opacity-100 lg:group-hover:pointer-events-auto lg:group-focus-within:opacity-100 lg:group-focus-within:pointer-events-auto lg:opacity-0 lg:pointer-events-none min-w-[100%] bg-white flex lg:flex-row flex-col lg:gap-0 gap-[1rem] lg:mb-0 mb-[.6rem] lg:border-t border-t-light-primary transition z-40",
+    megaMenuCol: "flex flex-col flex-1 lg:border-r border-r-light-primary",
+    megaMenuColTitle: "inline-block lg:px-[1.5rem] lg:mb-[.6rem] lg:px-[2.3rem] px-[1.4rem] py-[1rem] w-full text-[1.5rem] font-semibold uppercase",
+    subMenu: "flex flex-col gap-[1rem]",
+    subMenuLink: "block lg:px-[2.3rem] px-[3.4rem] text-[1.6rem] w-full hover:text-primary",
+    megaMenuColImage: "lg:block hidden ml-[2rem]",
+    megaMenuImage: "rounded-[.6rem] h-[32rem] w-[25rem] object-cover transition-opacity duration-500",
     burgerButton: "lg:hidden cursor-pointer as--hover-filter-primary transition-all",
     closeIcon: "w-4 h-4",
     burgerIcon: "w-3 h-3",
@@ -33,6 +37,7 @@ const styles = tv({
     searchBar: "searchbar fixed top-0 left-0 w-full flex flex-col bg-white z-1000 px-[3rem] py-[4rem] shadow-md items-center gap-2 transition-transform duration-300",
     closeSearchBarButton: "cursor-pointer as--hover-filter-primary",
     searchForm: "flex sm:items-stretch items-center sm:flex-row flex-col gap-y-[1.4rem] w-full max-w-[85rem]",
+    searchInput: "h-full",
     submitSearchButton: "w-fit bg-primary hover:bg-dark-primary",
   },
   variants: {
@@ -47,11 +52,11 @@ const styles = tv({
     active: {
       true: {
         navLink: "text-dark-primary",
-        subLink: "text-dark-primary",
+        megaMenuColTitle: "text-dark-primary",
       },
       false: {
         navLink: "hover:text-primary",
-        subLink: "hover:text-primary",
+        megaMenuColTitle: "hover:text-primary",
       },
     },
     searchOpen: {
@@ -62,9 +67,21 @@ const styles = tv({
         searchBar: "translate-y-[-110%]",
       },
     },
+    visible: {
+      true: {
+        megaMenuImage: "opacity-100",
+      },
+      false: {
+        megaMenuImage: "opacity-0",
+      },
+    },
   },
 });
-const { navWrapper, navList, navItem, navLink, subMenu, subLink, burgerButton, closeIcon, burgerIcon, searchButton, searchIcon, searchBar, closeSearchBarButton, searchForm, submitSearchButton } = styles();
+const { navWrapper, navList, navItem, navLink, megaMenu, megaMenuCol, megaMenuColTitle, subMenu, subMenuLink, megaMenuColImage,
+  megaMenuImage, burgerButton, closeIcon, burgerIcon, searchButton, searchIcon, searchBar, closeSearchBarButton, searchForm,
+  searchInput, submitSearchButton,
+
+} = styles();
 
 
 const Navigation = ({ className = '' }: { className?: string }) => {
@@ -121,11 +138,11 @@ const Navigation = ({ className = '' }: { className?: string }) => {
                 )}
 
                 {heading?.hasShopMegamenu && productCategories && (
-                  <ul className={subMenu()}>
+                  <ul className={megaMenu()}>
                     {productCategories.map((category) => (
                       <li
                         key={category.documentId}
-                        className="flex flex-col flex-1 lg:border-r border-r-light-primary"
+                        className={megaMenuCol()}
                       >
                         <CustomLink
                           href={`/boutique/categorie/${category.slug}`}
@@ -136,11 +153,11 @@ const Navigation = ({ className = '' }: { className?: string }) => {
                               setTimeout(() => setIsImageVisible(true), 8);
                             }
                           }}
-                          className={subLink({ active: pathname === `/categorie/${category.slug}` })}>
+                          className={megaMenuColTitle({ active: pathname === `/categorie/${category.slug}` })}>
                           {category.name}
                         </CustomLink>
 
-                        <ul className="flex flex-col gap-[1rem]">
+                        <ul className={subMenu()}>
                           {category.product_subcategories?.map((sub) => (
                             <li key={sub.documentId}>
                               <CustomLink
@@ -154,7 +171,7 @@ const Navigation = ({ className = '' }: { className?: string }) => {
                                   setIsImageVisible(false);
                                   setTimeout(() => setIsImageVisible(true), 8);
                                 }}
-                                className="block lg:px-[2.3rem] px-[3.4rem] text-[1.6rem] w-full hover:text-primary"
+                                className={subMenuLink()}
                               >
                                 {sub.name}
                               </CustomLink>
@@ -165,17 +182,14 @@ const Navigation = ({ className = '' }: { className?: string }) => {
                     ))}
 
                     {(hoveredImage || productCategories[0]?.illustration?.url) && (
-                      <li className="lg:block hidden ml-[2rem]">
+                      <li className={megaMenuColImage()}>
                         <Image
                           key={hoveredImage || "default"}
                           src={`${process.env.NEXT_PUBLIC_API_URL}${hoveredImage || productCategories[0].illustration.url}`}
                           alt="Illustration"
                           width={250}
                           height={320}
-                          className={clsx(
-                            "rounded-[.6rem] h-[32rem] w-[25rem] object-cover transition-opacity duration-500",
-                            isImageVisible ? "opacity-100" : "opacity-0"
-                          )} />
+                          className={megaMenuImage({ visible: isImageVisible })} />
                       </li>
                     )}
                   </ul>
@@ -212,7 +226,7 @@ const Navigation = ({ className = '' }: { className?: string }) => {
             type="text"
             placeholder="Rechercher une création, une catégorie..."
             label="Rechercher un terme"
-            className="h-full"
+            className={searchInput()}
             hasLabelHidden
             autoFocus={isSearchOpen}
             tabIndex={isSearchOpen ? 0 : -1}
